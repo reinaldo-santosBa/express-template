0cdfb0bc87b3b3fe096d3528dc85f5cfb16d257a
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-03-15 14:52:39 -0300

    refactor: att to bohr

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -1,4 +1,30 @@
-import { AppServer } from './config/appServer';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { ErrorHandler } from './middlewares/errorHandle';
+import { TokenFcmRoutes } from './routes/tokenFCM';
+import { UserRoutes } from './routes/user';
+import { AppError } from './error/appError';
+import { dbConfig } from './config/dbConfig';
+dotenv.config();
+new dbConfig();
 
-export const server = new AppServer();
-export default server;
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+app.use('/token', TokenFcmRoutes);
+app.use('/user', UserRoutes);
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new AppError('Route not found', 404);
+    next(error);
+});
+
+app.use(ErrorHandler);
+const port = Number(process.env.PORT || 3000);
+if (!module.parent) {
+    app.listen(3000);
+    console.log("Express started on port 3000");
+}
+//export const server = new AppServer();
+export default app;
